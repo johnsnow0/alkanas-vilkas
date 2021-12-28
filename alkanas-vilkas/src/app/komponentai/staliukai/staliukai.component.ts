78134b5a6135c1c19d96d0c7b8ca215184d1f898
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-12-28 21:30:31 +0200

    pirma versija

---
+++ alkanas-vilkas/src/app/komponentai/staliukai/staliukai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staliukai',
+  templateUrl: './staliukai.component.html',
+  styleUrls: ['./staliukai.component.css']
+})
+export class StaliukaiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
